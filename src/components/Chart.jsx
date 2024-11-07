3381440978435122a36e73641d45a425ed90ba9c
ACORBERAND <antoinecobertondefleury@gmail.com>
2024-11-07 11:50:49 +0100

    chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,32 +1,64 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
+import Data from "../data/mix-energie-gkwh.json"; // Assurez-vous que votre JSON est correctement importé
 
 // Enregistrer les composants utilisés de Chart.js
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const LineChart = () => {
-  const [data, setData] = useState([]);
   const [showData, setShowData] = useState(true);
 
-  useEffect(() => {
-    // Charger les données depuis le fichier JSON
-    fetch("/data.json")
-      .then((response) => response.json())
-      .then((jsonData) => setData(jsonData))
-      .catch((error) => console.error("Erreur lors du chargement des données : ", error));
-  }, []);
+  // Extraire les années et les valeurs pour chaque source d'énergie
+  const years = Data.Years.map((item) => item.year); // Utilisation des années de l'énergie pétrolière comme labels communs
 
-  // Construire les données pour le graphique
+  // Convertir la chaîne "value" en nombre et récupérer les années pour chaque source d'énergie
+  const convertToNumber = (value) => parseInt(value.replace('g/Kwh', '').trim());
+
   const chartData = {
-    labels: data.map((item) => item.date),
+    labels: years, // Années comme labels
     datasets: [
       {
-        label: "Nucléaire",
-        data: showData ? data.map((item) => item.value) : [],
-        borderColor: "rgb(75, 192, 192)",
-        backgroundColor: "rgba(75, 192, 192, 0.2)",
-        tension: 0.2,
+        label: "Pétrole", // Label de la série pour le pétrole
+        data: showData ? Data.Oil.map((item) => convertToNumber(item.value)) : [], // Données pour le pétrole
+        borderColor: "rgb(29, 90, 114)", // Couleur de la ligne
+        backgroundColor: "rgba(29, 90, 114, 0.2)", // Couleur de fond sous la ligne
+        tension: 0.2, // Détermine la courbure de la ligne
+      },
+      {
+        label: "Nucléaire", // Label de la série pour le nucléaire
+        data: showData ? Data.Nuclear.map((item) => convertToNumber(item.value)) : [], // Données pour le nucléaire
+        borderColor: "rgb(223, 192, 36)", // Couleur de la ligne
+        backgroundColor: "rgba(223, 192, 36, 0.2)", // Couleur de fond sous la ligne
+        tension: 0.2, // Détermine la courbure de la ligne
+      },
+      {
+        label: "Gaz", // Label de la série pour le gaz
+        data: showData ? Data.Gaz.map((item) => convertToNumber(item.value)) : [], // Données pour le gaz
+        borderColor: "rgb(144, 115, 87)", // Couleur de la ligne
+        backgroundColor: "rgba(144, 115, 87, 0.2)", // Couleur de fond sous la ligne
+        tension: 0.2, // Détermine la courbure de la ligne
+      },
+      {
+        label: "Solaire", // Label de la série pour le solaire
+        data: showData ? Data.Solar.map((item) => convertToNumber(item.value)) : [], // Données pour le solaire
+        borderColor: "rgb(167, 46, 46)", // Couleur de la ligne
+        backgroundColor: "rgba(167, 46, 46, 0.2)", // Couleur de fond sous la ligne
+        tension: 0.2, // Détermine la courbure de la ligne
+      },
+      {
+        label: "Éolienne", // Label de la série pour l'éolien
+        data: showData ? Data.Eolian.map((item) => convertToNumber(item.value)) : [], // Données pour l'éolien
+        borderColor: "rgb(85, 164, 69)", // Couleur de la ligne
+        backgroundColor: "rgba(85, 164, 69, 0.2)", // Couleur de fond sous la ligne
+        tension: 0.2, // Détermine la courbure de la ligne
+      },
+      {
+        label: "Charbon", // Label de la série pour le charbon
+        data: showData ? Data.Coal.map((item) => convertToNumber(item.value)) : [], // Données pour le charbon
+        borderColor: "rgb(54, 54, 54)", // Couleur de la ligne
+        backgroundColor: "rgba(54, 54, 54, 0.2)", // Couleur de fond sous la ligne
+        tension: 0.2, // Détermine la courbure de la ligne
       },
     ],
   };
@@ -60,7 +92,7 @@ const LineChart = () => {
 
   return (
     <div>
-      <h2>Évolution de la valeur en g/kWh</h2>
+      <h2>Émissions de CO2 (g/kWh) des Différentes Sources Énergétiques au Cours du Temps</h2>
       <Line data={chartData} options={options} />
     </div>
   );
