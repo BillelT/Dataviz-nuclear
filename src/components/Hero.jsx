d7cd833d17d6288ffbd4c1186834bd531be8dbdf
Simon Daguet-Kargl <simondaguetkargl@gmail.com>
2024-11-08 14:53:06 +0100

    ajout animations gsap part1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,46 @@
-import { useRef } from "react";
-// import gsap from "gsap";
+import { useRef, useEffect } from "react";
 import "../stylesheets/Hero.scss";
+import { gsap } from "gsap";
+import SplitType from "split-type";
 
 function Hero() {
-  const firstPartTitle = useRef();
-  const secondPartTitle = useRef();
+  const textToSplit = useRef(null);
+  const leRef = useRef(null);
+
+  useEffect(() => {
+    if (textToSplit.current && leRef.current) {
+      const text = new SplitType(textToSplit.current, { types: "chars" });
+
+      const tl = gsap.timeline();
+      console.log(leRef.current, leRef);
+
+      tl.from(text.chars, {
+        duration: 0.8,
+        y: 70,
+        opacity: 0,
+        stagger: 0.04,
+        ease: "power2.out",
+        delay: 0.5,
+      });
+    }
+  }, []);
 
   return (
-    <>
-      <section className="darkgreen-bg container flex flex-center flex-d-col p-top-bot-256 row-gap-32">
-        <span className="text-center hero-text">Le</span>
+    <section className="darkgreen-bg container flex flex-center flex-d-col p-top-bot-256 row-gap-32">
+      <div className="span-container text-center hero-text">
+        <span className="" ref={leRef}>
+          Le
+        </span>
+      </div>
+      <div className="h1-container">
         <h1 className="lime-text text-center archivo font-weight-900">
-          <span ref={firstPartTitle} className="archivo font-weight-900">
-            NUC
-          </span>
-          <span ref={secondPartTitle} className="archivo font-weight-900">
-            LÉAIRE
+          <span ref={textToSplit} className="archivo font-weight-900">
+            NUCLÉAIRE
           </span>
         </h1>
-        <span className="text-center hero-text">est-il la solution ?</span>
-      </section>
-    </>
+      </div>
+      <span className="text-center hero-text">est-il la solution ?</span>
+    </section>
   );
 }
 
