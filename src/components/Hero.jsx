fe1ab952555a53620d47f8382791a05115dacbbf
Billel T <148473122+BillelT@users.noreply.github.com>
2024-11-08 09:05:40 +0100

    debug build

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,22 @@
+import { useRef } from "react";
+// import gsap from "gsap";
 import "../stylesheets/Hero.scss";
 
 function Hero() {
+  const firstPartTitle = useRef();
+  const secondPartTitle = useRef();
+
   return (
     <>
       <section className="darkgreen-bg container flex flex-center flex-d-col p-top-bot-256 row-gap-32">
         <span className="text-center hero-text">Le</span>
         <h1 className="lime-text text-center archivo font-weight-900">
-          <span className=" archivo font-weight-900">NUC</span>LÉAIRE
+          <span ref={firstPartTitle} className="archivo font-weight-900">
+            NUC
+          </span>
+          <span ref={secondPartTitle} className="archivo font-weight-900">
+            LÉAIRE
+          </span>
         </h1>
         <span className="text-center hero-text">est-il la solution ?</span>
       </section>
